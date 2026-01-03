ff702364f01a4499693b94d09be8ddcd397c5648
ThePotat0 <markbrusman@gmail.com>
2026-01-03 14:41:51 +0300

    Scripts folder added
    Scripts folder and LevelMovement script added

---
+++ Assets/Scripts/LevelMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMovement : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _levelFragments;
+    private List<GameObject> _leftRow;
+    private List<GameObject> _righttRow;
+    private List<GameObject> _upperRow;
+    private List<GameObject> _lowerRow;
+
+    private void Start()
+    {
+        
+    }
+
+    private void UpperMovement() 
+    {
+
+    }
+
+    private void LowerMovement() 
+    {
+
+    }
+
+    private void LeftMovement() 
+    {
+
+    }
+
+    private void RightMovement() 
+    {
+
+    }
+}
