6e78c2b77bc82e13efe9202acb5877db4e822ae3
ThePotat0 <markbrusman@gmail.com>
2026-01-04 13:36:15 +0300

    First level work in progress

--- Assets/Scripts/Levels/Tile_GravityDoor.cs
+++
@@ -1,5 +0,0 @@
-using UnityEngine;
-
-public class Tile_GravityDoor : TileController
-{
-}
---
+++ Assets/Scripts/Levels/Tile_SimpleToggle.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Tile_SimpleToggle : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
