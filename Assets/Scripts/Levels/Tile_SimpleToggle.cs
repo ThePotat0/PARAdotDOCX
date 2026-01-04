f01b96dcbc3038d7cf729754668338b365bb43e0
ThePotat0 <markbrusman@gmail.com>
2026-01-04 17:34:18 +0300

    Some levls complited
    Complited levels: Level_1.1 (button), Level 4.4 (gravity door)

--- Assets/Scripts/Levels/Tile_SimpleToggle.cs
+++ Assets/Scripts/Levels/Tile_SimpleToggle.cs
@@ -5,7 +5,6 @@ public class Tile_SimpleToggle : MonoBehaviour
     [SerializeField] private int _levelID;
     private LevelMovement _leveloMvement;
     private bool _playerOnTrigger = false;
-    private bool _isRotated = false;
 
     private void Start()
     {
@@ -24,14 +23,14 @@ public class Tile_SimpleToggle : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.E) && _playerOnTrigger && !_isRotated) 
+        if (Input.GetKeyDown(KeyCode.E) && _playerOnTrigger) 
         {
+            Destroy(gameObject);
             switch (_levelID) 
             {
                 case 1: 
                     {
                         _leveloMvement.RightRotation();
-                        _isRotated = true;
                         break;
                     }
                 case 4: 
@@ -39,7 +38,6 @@ public class Tile_SimpleToggle : MonoBehaviour
                         var player = FindAnyObjectByType<PlayerMovement>();
                         player.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
                         _leveloMvement.LowerRotation();
-                        _isRotated = true;
                         break;
                     }
             }
