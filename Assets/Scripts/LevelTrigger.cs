7b51db3210c5d17c992f4951f122e8f99e356afe
Eugene Maslenkov <yujiriwc@gmail.com>
2026-01-04 21:09:30 +0300

    scroll level wen user inputs 22:22 into the _clock field_
    closes #4

--- Assets/Scripts/LevelTrigger.cs
+++ Assets/Scripts/LevelTrigger.cs
@@ -7,12 +7,16 @@ using UnityEngine;
 //   возможно стоит зарефакторить и использовать "направление"? down/right?
 // _window - компонент на странице, сейчас рекомендуется использовать "интерфейс комухтера", который
 //   используется что бы убедиться что _triggerComponnent виден пользователю
-// вызвать LevelTrigger.ForceMoveTo
+// вызвать LevelTrigger.ForceMoveTo или через кнопку вызывать LevelTrigger.ForceMoveToForButtons
 // скрипт выполняет движение один раз, после выполнения он удаляет _trigger дабавленный внутри Start
 
 // Для реакции на "кнопку звука" - этот скрипт нужно добавить как компонент к кнопке ToggleSound
 // проставить все необходимые поля и затем
-// в On Click () кнопки добавить вызов LevelTrigger.ForceMoveTo
+// в On Click () кнопки добавить вызов LevelTrigger.ForceMoveToForButtons
+
+// Для реакции на "время" - этот скрипт нужно добавить как компонент объекту который
+// вызовет .ForceMoveTo
+// при этом нужно этот скрипт верно настроить (смотри инструкцию выше)
 public class LevelTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject _triggerComponent;
@@ -25,20 +29,27 @@ public class LevelTrigger : MonoBehaviour
     {
         if (_triggerComponent == null)
         {
-            Debug.Log($"SET Trigger Component and Level ID for {gameObject.name} button!!!");
+            Debug.Log($"SET Trigger Component and Level ID for {gameObject.name} object!!!");
         } else
         {
             _trigger = _triggerComponent.GetComponent<Tile_SimpleToggle>();
         }
     }
 
-    public void ForceMoveTo()
+    public void ForceMoveToForButtons() // buttons do not accept functions that returns values
+    {
+        ForceMoveTo();
+    }
+
+    public bool ForceMoveTo()
     {
         if (_trigger != null && IsTriggerInWindow())
         {
             _trigger.ForceMoveTo(_levelID);
             _trigger = null;
+            return true; // this helps check is it moved successfully
         }
+        return false;
     }
 
     private bool IsTriggerInWindow()
