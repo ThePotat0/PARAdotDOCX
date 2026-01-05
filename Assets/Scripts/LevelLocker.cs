a2a113d35cff4a99abb19cdaad720d567944d908
ThePotat0 <markbrusman@gmail.com>
2026-01-05 18:38:28 +0300

    Canvas scale fixed

---
+++ Assets/Scripts/LevelLocker.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class LevelLocker : MonoBehaviour
+{
+    [SerializeField] private GameObject _wallFragment;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) 
+        {
+            _wallFragment.transform.localScale = new Vector3(_wallFragment.transform.localScale.x + 3, _wallFragment.transform.localScale.y, _wallFragment.transform.localScale.z);
+        }
+    }
+}
